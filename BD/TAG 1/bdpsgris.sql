2d5dc6d8caf419c112d4803f14acd0bf90b4f7bb
ThallesNonato1123 <eusouthalles@gmail.com>
2020-02-16 00:30:24 -0300

    Adicionando pasta do banco de dados e as tags

---
+++ BD/TAG 1/bdpsgris.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE grisps;
+
+USE grisps;
+
+CREATE TABLE candidato(
+    dre INT NOT NULL,
+    nome VARCHAR(80) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    telegram VARCHAR(15) NOT NULL,
+    `nota questionario` FLOAT,
+    PRIMARY KEY(dre)
+);
+
+CREATE TABLE `representante gris`(
+    dre INT NOT NULL, 
+    nome VARCHAR(80) NOT NULL,
+    materia VARCHAR(80),
+    PRIMARY KEY(dre)
+); 
+
+CREATE TABLE tag(
+    id INT AUTO_INCREMENT NOT NULL,
+    prazo DATE NOT NULL,
+    nome VARCHAR(80) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE candidato_tag(
+    id INT AUTO_INCREMENT NOT NULL,
+    candidato INT NOT NULL,
+    tag INT NOT NULL,
+    `data de entrega` DATE,
+    PRIMARY KEY(id),
+    FOREIGN KEY(candidato) REFERENCES candidato(dre),
+    FOREIGN KEY(tag) REFERENCES tag(id)
+);
